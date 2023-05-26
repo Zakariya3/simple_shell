a5bee484466ebedfef55e22da3d10ce33eb00a65
Zakariya3 <zakariaelgamh3@gmail.com>
2023-05-26 08:22:02 -0700

    red

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function that prints a message to the console
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+
